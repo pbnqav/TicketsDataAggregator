c38ac6e067c3cd914545ed70af211f3e1c0dd778
Pedro Boa-Nova <pedro.boanova@qavalue.com>
2023-11-17 10:59:46 +0000

    Projeto inicial

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig;
+
+namespace TicketsDataAggregator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string TicketsFolder = @"c:\ourcinema\tickets";
+
+            try
+            {
+                var ticketsAggregator = new TicketsAggregator(
+                    TicketsFolder);
+
+                ticketsAggregator.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception occured."+
+                    "Exception message: " + ex.Message);
+            }
+        }
+    }
+
+    public class TicketsAggregator
+    {
+        private string _ticketsFolder;
+
+        public TicketsAggregator(string ticketsFolder)
+        {
+            _ticketsFolder = ticketsFolder;
+        }
+
+        public void Run()
+        {
+            using (PdfDocument document = PdfDocument.Open(@"C:\my-file.pdf"))
+            {
+                int pageCount = document.NumberOfPages;
+
+                // Page number starts from 1, not 0.
+                Page page = document.GetPage(1);
+
+                decimal widthInPoints = page.Width;
+                decimal heightInPoints = page.Height;
+
+                string text = page.Text;
+            }
+        }
+    }
+}
